5311cfe182693383fd2c6a725c374d0a7879aebc
Nekrocow <nekrocow@gmail.com>
2022-11-22 13:20:03 -0300

    Config Mongo for localhost

--- server/app.js
+++ server/app.js
@@ -1,31 +1,33 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const { router } = require("./routes");
-const { json } = require("express");
-const { handleHttpError } = require("./utils/handleError");
+const { router } = require('./routes')
+const { json } = require('express')
+const { handleHttpError } = require('./utils/handleError')
 
 // Init express
 
-const app = express();
+const app = express()
 
-//Api config;
-app.use(cors());
-app.use(json());
-app.use(express.urlencoded({ extended: true }));
+//Api config
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //Serving static files /fixthis later
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Set template engine 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 app.set('services', path.join(__dirname, "./services/emails','template-engine'"))
 
 
 //Index Router
 
-app.use("/api", router);
+app.use('/api', router)
 
 
 // Error endpoint not found 
