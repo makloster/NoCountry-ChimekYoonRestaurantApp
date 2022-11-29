f5d5eec6895e1056375c68d4490eae963bbd2a6e
AlexanderC <alex88_upt@hotmail.com>
2022-11-29 18:05:17 -0500

    Table Icon y Confirmation Modals

--- client/src/components/ShoppingCart/FootherBtns.js
+++ client/src/components/ShoppingCart/FootherBtns.js
@@ -1,5 +1,6 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
+import ConfirmationModal from '../ConfirmationModals/ConfirmationModal';
 
 const FootherBtns = (props) => {
   const {
@@ -10,13 +11,22 @@ const FootherBtns = (props) => {
     styleTextConfirmBtn,
     textAddBtn,
     textConfirmBtn,
+    confirmation,
+    onPressConfirmation,
+    setConfirmation
+    
   } = props;
+
+  
   return (
     <View style={styleContainerFootherBtns}>
+      <ConfirmationModal confirmation={confirmation} setConfirmation={setConfirmation}/>
       <TouchableOpacity style={styleAddBtn}>
         <Text style={styleTextAddBtn}>{textAddBtn}</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styleConfirmBtn}>
+      <TouchableOpacity 
+        style={styleConfirmBtn}
+        onPress={onPressConfirmation} >
         <Text style={styleTextConfirmBtn}>{textConfirmBtn}</Text>
       </TouchableOpacity>
     </View>
