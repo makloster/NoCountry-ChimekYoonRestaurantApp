3d5027d36bea4fd52f295bacf28e73697332fa5a
Nekrocow <nekrocow@gmail.com>
2022-12-01 08:50:41 -0300

    waiter backup

--- server/apiServices/waiters/waiters.routes.js
+++ server/apiServices/waiters/waiters.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { create, login } = require('./users.controller')
 const waiterRouter = express.Router()
 
-waiterRouter.post('/register', userValidator, create)
+waiterRouter.post('/register', create)
 waiterRouter.post('/login', login)
 
 module.exports = { waiterRouter }
