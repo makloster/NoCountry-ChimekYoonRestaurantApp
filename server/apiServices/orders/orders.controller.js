d3af1d6de35560cadf619fd5d3ce3ee291895877
Nekrocow <nekrocow@gmail.com>
2022-12-07 21:57:48 -0300

    fix: fix addCategory & add populate to getters

--- server/apiServices/orders/orders.controller.js
+++ server/apiServices/orders/orders.controller.js
@@ -1,7 +1,6 @@
 const { handleHttpError } = require('../../utils/handleError')
 const { Order } = require('./orders.model')
-
-const projection = { createdAt: 0, updatedAt: 0, __v: 0, avaliable: 0 }
+const { projection } = require('../../utils/utils')
 
 const getOrders = async (req, res) => {
   try {
@@ -9,6 +8,12 @@ const getOrders = async (req, res) => {
       .find({}, projection)
       .populate('waiter', projection)
       .populate('items')
+      .populate({
+        path: 'items',
+        populate: {
+          path: 'item'
+        }
+      })
     res.json(orderList)
   } catch (error) {
     handleHttpError(res, 'ERROR_GET_ORDERS', 500)
